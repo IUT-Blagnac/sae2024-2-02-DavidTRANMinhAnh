ff67bfc7ba79b352a8198d84166a0441874dda8f
DavidTranMinhAnh <davidtran31770@gmail>
2024-06-14 19:49:10 +0200

    Update Algo.java
    correction code efficace

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -1,28 +1,27 @@
 package iut.sae.algo;
 
-
-public class Algo{
-    public static String RLE(String in){
+public class Algo {
+    public static String RLE(String in) {
         if (in == null || in.isEmpty()) {
             return "";
         }
         StringBuilder resultat = new StringBuilder();
-        int cpt =1;
-        for (int i = 0; i < in.length(); i++) {
-            if (in.charAt(i)== in.charAt(i-1)) {
+        int cpt = 1;
+        for (int i = 1; i < in.length(); i++) {
+            if (in.charAt(i) == in.charAt(i - 1)) {
                 cpt++;
             } else {
-                resultat.append(cpt).append(in.charAt(i-1));
+                resultat.append(cpt).append(in.charAt(i - 1));
                 cpt = 1;
             }
         }
-        resultat.append(cpt).append(in.charAt(in.length()-1));
+        resultat.append(cpt).append(in.charAt(in.length() - 1));
         return resultat.toString();
     }
 
-    public static String RLE(String in, int iteration) throws AlgoException{
+    public static String RLE(String in, int iteration) throws AlgoException {
         if (iteration < 1) {
-            throw new AlgoException("Iteration > 1");
+            throw new AlgoException("Iteration doit être >= 1");
         }
         String resultat = in;
         for (int i = 0; i < iteration; i++) {
@@ -31,34 +30,31 @@ public class Algo{
         return resultat;
     }
 
-    public static String unRLE(String in) throws AlgoException{
+    public static String unRLE(String in) throws AlgoException {
         if (in == null || in.isEmpty()) {
             return "";
         }
         StringBuilder resultat = new StringBuilder();
-        int cpt =1;
-        for(char c : in.toCharArray()){
-            if(Character.isDigit(c)){
-                cpt = cpt*10+Character.getNumericValue(c);
+        int cpt = 0;
+        for (char c : in.toCharArray()) {
+            if (Character.isDigit(c)) {
+                cpt = cpt * 10 + Character.getNumericValue(c);
             } else {
                 for (int i = 0; i < cpt; i++) {
                     resultat.append(c);
                 }
-                cpt =0;
+                cpt = 0;
             }
         }
         return resultat.toString();
-
     }
 
-    public static String unRLE(String in, int iteration) throws AlgoException{
-        if (iteration < 1) throw new AlgoException("Iteration must be >= 1");
+    public static String unRLE(String in, int iteration) throws AlgoException {
+        if (iteration < 1) throw new AlgoException("Iteration devrait être >= 1");
         String result = in;
         for (int i = 0; i < iteration; i++) {
             result = unRLE(result);
         }
         return result;
-
     }
 }
-
