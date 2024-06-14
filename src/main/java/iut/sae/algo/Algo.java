1409e0219909c6d5762d5191684e245ef2a67bf7
DavidTRAN <davidtran31770@gmail.com>
2024-06-14 19:28:25 +0200

    Updated Code + ReadMe
    Efficacité + Modification du ReadMe

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -3,24 +3,61 @@ package iut.sae.algo;
 
 public class Algo{
     public static String RLE(String in){
-        // Provide your algo here
-        return "NotYetImplemented";
+        if (in == null || in.isEmpty()) {
+            return "";
+        }
+        StringBuilder resultat = new StringBuilder();
+        int cpt =1;
+        for (int i = 0; i < in.length(); i++) {
+            if (in.charAt(i)== in.charAt(i-1)) {
+                cpt++;
+            } else {
+                resultat.append(cpt).append(in.charAt(i-1));
+                cpt = 1;
+            }
+        }
+        resultat.append(cpt).append(in.charAt(in.length()-1));
+        return resultat.toString();
     }
 
     public static String RLE(String in, int iteration) throws AlgoException{
-        // Provide your algo here
-        return "NotYetImplemented";
+        if (iteration < 1) {
+            throw new AlgoException("Iteration > 1");
+        }
+        String resultat = in;
+        for (int i = 0; i < iteration; i++) {
+            resultat = RLE(resultat);
+        }
+        return resultat;
     }
 
     public static String unRLE(String in) throws AlgoException{
-        // Provide your algo here
-        return "NotYetImplemented";
+        if (in == null || in.isEmpty()) {
+            return "";
+        }
+        StringBuilder resultat = new StringBuilder();
+        int cpt =1;
+        for(char c : in.toCharArray()){
+            if(Character.isDigit(c)){
+                cpt = cpt*10+Character.getNumericValue(c);
+            } else {
+                for (int i = 0; i < cpt; i++) {
+                    resultat.append(c);
+                }
+                cpt =0;
+            }
+        }
+        return resultat.toString();
 
     }
 
     public static String unRLE(String in, int iteration) throws AlgoException{
-        // Provide your algo here
-        return "NotYetImplemented";
+        if (iteration < 1) throw new AlgoException("Iteration must be >= 1");
+        String result = in;
+        for (int i = 0; i < iteration; i++) {
+            result = unRLE(result);
+        }
+        return result;
 
     }
 }
